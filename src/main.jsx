1017c16865d6ad07515d36521963b8d1608e41dd
sebas <carrenosebastian54@gmail.com>
2023-06-15 17:01:53 -0500

    One site page excel

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import AsignacionMetas from "./routes/AsignacionMetas";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import SubirExcelMetas from "./routes/SubirExcelMetas";
-import ErrorPage from "./routes/ErrorPage";
 const theme = createTheme({
     typography: {
         fontFamily: [
@@ -37,28 +34,11 @@ const theme = createTheme({
     },
 });
 
-const router = createBrowserRouter([
-    {
-        path: "/asignacion-metas",
-        element: <AsignacionMetas />,
-        errorElement: <ErrorPage />,
-    },
-    {
-        path: "/subir-excel-metas",
-        element: <SubirExcelMetas />,
-        errorElement: <ErrorPage />,
-    },
-    {
-        path: "*",
-        element: <ErrorPage />,
-    },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <RouterProvider router={router} />
+            <SubirExcelMetas />
         </ThemeProvider>
     </React.StrictMode>
 );
