928a480bd9d8b5d72d07ae603e1981df4703ae8b
sebas <carrenosebastian54@gmail.com>
2023-07-06 18:00:18 -0500

    Advanced the edit functionality

--- src/routes/AnalisisMetas.jsx
+++ src/routes/AnalisisMetas.jsx
@@ -1,7 +1,7 @@
 import { Container, Typography } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import SnackbarAlert from "../components/SnackbarAlert";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const AnalisisMetas = () => {
     const [openSnackbar, setOpenSnackbar] = useState(false);
@@ -11,22 +11,22 @@ const AnalisisMetas = () => {
     const [rows, setRows] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch("https://intranet.cyc-bpo.com/getSessionValue.php");
-            const data = await response.text();
-            console.log(data);
-            if (data === "No ha accedido al sistema") {
-                window.location.href = "https://intranet.cyc-bpo.com/";
-            } else {
-                setCoordinator(data);
-            }
-        };
-        fetchData();
+        // const fetchData = async () => {
+        //     const response = await fetch("https://intranet.cyc-bpo.com/getSessionValue.php");
+        //     const data = await response.text();
+        //     console.log(data);
+        //     if (data === "No ha accedido al sistema") {
+        //         window.location.href = "https://intranet.cyc-bpo.com/";
+        //     } else {
+        //         setCoordinator(data);
+        //     }
+        // };
+        // fetchData();
 
         const handleSave = async () => {
             try {
-                const encodedCoordinator = encodeURIComponent("FAVIAN SIERRA");
-                const response = await fetch(`https://api.cyc-bpo.com/goals/?coordinator=${encodedCoordinator}`, {
+                //const encodedCoordinator = encodeURIComponent("FAVIAN SIERRA");
+                const response = await fetch(`https://api.cyc-bpo.com/goals/`, {
                     method: "GET",
                 });
 
@@ -56,18 +56,19 @@ const AnalisisMetas = () => {
 
                 if (response.status === 200) {
                     const data = await response.json();
+                    console.log(data);
                     const modifiedData = data.map((row) => {
                         return {
                             ...row,
-                            created_at: row.created_at.substring(0, 10),
+                            last_update: row.last_update.substring(0, 10),
                             accepted: row.accepted == 0 ? "Rechazada" : row.accepted == 1 ? "Aceptada" : "En espera",
-                            clean_desk: row.clean_desk === "" ? "En Ejecución" : row.clean_desk,
-                            quality: row.quality === "" ? "En Ejecución" : row.quality,
-                            result: row.result === "0.00%" ? "En Ejecución" : row.result,
-                            total: row.total === "" ? "En Ejecución" : row.total,
+                            clean_desk: row.clean_desk === "" ? "En Espera" : row.clean_desk,
+                            quality: row.quality === "" ? "En Espera" : row.quality,
+                            result: row.result === "0.00%" ? "En Espera" : row.result,
+                            total: row.total === "" ? "En Espera" : row.total,
                             accepted_execution:
-                                row.total == null && row.accepted_execution == null
-                                    ? "En ejecucion"
+                                row.total == "" && row.accepted_execution == null
+                                    ? ""
                                     : row.accepted_execution == 0
                                     ? "Rechazada"
                                     : row.accepted_execution == 1
@@ -87,13 +88,13 @@ const AnalisisMetas = () => {
     const columns = [
         { field: "cedula", headerName: "Cedula", width: 140 },
         { field: "campaign", headerName: "Campaña", width: 140 },
-        { field: "clean_desk", headerName: "Clean Desk", width: 140 },
+        { field: "clean_desk", headerName: "Clean Desk", width: 140, editable: true },
         { field: "quality", headerName: "Calidad", width: 140 },
         { field: "result", headerName: "Resultado", width: 140 },
         { field: "total", headerName: "Total", width: 140 },
-        { field: "created_at", headerName: "Fecha de Creación", width: 140 },
-        { field: "accepted", headerName: "Estado de la Meta", width: 140 },
-        { field: "accepted_execution", headerName: "Estado de la Ejecucion de la Meta", width: 140 },
+        { field: "last_update", headerName: "Fecha de modificación", width: 140 },
+        { field: "accepted", headerName: "Aprobación de la Meta", width: 185 },
+        { field: "accepted_execution", headerName: "Aprobación de la Ejecución", width: 185 },
     ];
 
     const handleCloseSnackbar = (event, reason) => {
@@ -102,6 +103,42 @@ const AnalisisMetas = () => {
         }
         setOpenSnackbar(false);
     };
+
+    const handleProcessRowUpdateError = useCallback((error) => {
+        setOpenSnackbar(true);
+        setSnackbarSeverity("error");
+        setSnackbarMessage(error.message);
+    }, []);
+
+    const processRowUpdate = useCallback(async (newRow) => {
+        // Make the HTTP request to save in the backend
+        try {
+            const response = await fetch(`https://api.cyc-bpo.com/goals/${newRow.cedula}/`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    newRow,
+                }),
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                console.error(data);
+                throw new Error(response.statusText);
+            } else if (response.status === 200) {
+                const data = await response.json();
+                console.log(data);
+                setOpenSnackbar(true);
+                setSnackbarSeverity("success");
+                setSnackbarMessage("Meta actualizada correctamente");
+            }
+            return response;
+        } catch (error) {
+            console.error(error);
+        }
+    }, []);
 
     return (
         <Container
@@ -121,6 +158,8 @@ const AnalisisMetas = () => {
                 sx={{ maxHeight: "600px" }}
                 rows={rows}
                 columns={columns}
+                processRowUpdate={processRowUpdate}
+                onProcessRowUpdateError={handleProcessRowUpdateError}
                 slots={{ toolbar: GridToolbar }}
                 slotProps={{
                     toolbar: {
